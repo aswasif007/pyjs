bc75b1aaf8f54652bd2736fd9130aee9aeb5704d
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2020-09-07 20:55:15 +0600

    Strip-off async-await

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const py = require('./py');
 
-async function main() {
-  const msg = await py.hello_world('Lord Stark');
+py.hello_world('Lord Stark').then(msg => {
   console.log(msg);
-}
+});
 
-main();
 py.__stop__();
